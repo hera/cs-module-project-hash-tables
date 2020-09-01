3c850b9b4746606accb2d20dc87d3bdf780f363e
hera <a.heraimenka@gmail.com>
2020-09-01 16:04:34 -0700

    No dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,25 @@
 def no_dups(s):
-    # Your code here
+    word_list = s.split(" ")
 
+    # if there's only one word, return it
+    if len(word_list) == 1:
+        return s
 
+    uniques = {}
+
+    # False means a word hasn't been used yet
+    for word in word_list:
+        uniques[word] = False
+    
+    result_list = []
+
+    for word in word_list:
+        if word in uniques and uniques[word] == False:
+            result_list.append(word)
+            # mark this word as used. next time we'll skip it
+            uniques[word] = True
+
+    return " ".join(result_list)
 
 if __name__ == "__main__":
     print(no_dups(""))
